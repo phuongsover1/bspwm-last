cb0ee0554b541eb004281206289863279e34915e
Phuong Nguyen <phuongsover2@gmail.com>
2023-11-15 20:26:03 +0700

    update config

---
+++ neofetch/launch-neofetch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/bin/neofetch --backend kitty --source $HOME/.config/neofetch/image1.png --size '200px' --colors 5 5 7 4 4 7
+#/bin/neofetch --ascii --ascii_colors 5 --source $HOME/.config/neofetch/ascii --size '200px' --colors 5 5 7 4 4 7
--- polybar/scripts/pacman-updates.sh
+++ polybar/scripts/pacman-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ $(cat /etc/os-release | grep arch) ]] && yay -Qu | wc -l
+[[ $(cat /etc/os-release | grep arch) ]] && pacman -Qu | wc -l
 #[[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradeable | wc -l
 
 [[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradable 2>/dev/null | wc -l
